22a03e34b9cc70a22f43c2dd71ab4c68c6b60a8c
RollerCoaster4 <jannik.becker@bender.de>
2021-09-22 11:31:15 +0200

    11:31 22.09.2021

--- stockAPI.py
+++ stockAPI.py
@@ -3,6 +3,9 @@ import json
 
 
 stocks = ["ADDYY","ALV.DE"]
+cryptos = ["BTC"]
+
+
 def get_stock(stocks):
     back = ""
     for symbol in stocks:
@@ -12,4 +15,24 @@ def get_stock(stocks):
         back += data["01. symbol"] + " " + data["05. price"] + " " + data["10. change percent"] + "     "
     return back
 
-print(get_stock(stocks))
+
+def get_cryptos(cryptos):
+    back = ""
+    for symbol in cryptos:
+        url = f'https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol={symbol}&market=EUR&apikey=EP7VOHUF5XUQMVEX'
+        r = requests.get(url, verify=False)
+        data = json.loads(r.text)["Time Series (Digital Currency Daily)"]
+        res = list(data.keys())[1]
+        data = data[res]
+        open = round(float(data["1a. open (EUR)"]),1)
+        close = round(float(data["4a. close (EUR)"]),1)
+        dif = close - open
+        dif = round((dif/open * 100),1)
+        if dif > 0:
+            dif = "+" + str(dif) + "%"
+        else:
+            dif = str(dif) + "%"
+        back += symbol + " " + str(close) + " " + dif + "    "
+    return back
+
+print(get_cryptos(cryptos))
