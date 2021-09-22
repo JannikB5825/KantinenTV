2464c31d183250d47de7c978769e7c9d751d1f2e
RollerCoaster4 <jannik.becker@bender.de>
2021-09-22 10:43:45 +0200

    10:45 22.09.2021

--- stockAPI.py
+++ stockAPI.py
@@ -1,21 +1,15 @@
-#install yfinance
 import requests
 import json
-import yfinance as yf
 
-stocks = ["ADS"]
-for symbol in stocks:
-    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey=EP7VOHUF5XUQMVEX'
-    r = requests.get(url, verify=False)
-    data = json.loads(r.text)
-    data = data[list(data.keys())[1]]
-    res = list(data.keys())[0]
-    open = float(data[res]["1. open"])
-    close = float(data[res]["4. close"])
-    perc = round((((open - close)/open) * 100),2)
-    if perc > 0:
-        perc = "+" + str(perc) + "%"
-    else:
-        perc = str(perc) + "%"
 
-    print(stock + " " + str(close) + " " + perc)
+stocks = ["ADDYY","ALV.DE"]
+def get_stock(stocks):
+    back = ""
+    for symbol in stocks:
+        url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={symbol}&apikey=EP7VOHUF5XUQMVEX'
+        r = requests.get(url, verify=False)
+        data = json.loads(r.text)["Global Quote"]
+        back += data["01. symbol"] + " " + data["05. price"] + " " + data["10. change percent"] + "     "
+    return back
+
+print(get_stock(stocks))
