35c9cd161823d58f04fd1297fdca266704d2ee5a
RollerCoaster4 <jannik.becker@bender.de>
2021-10-06 10:49:10 +0200

    10:50 06.10.2021

--- resulution.py
+++ resulution.py
@@ -1,4 +1,36 @@
+#Import the required Libraries
+from tkinter import *
+import json as json_
+import requests
+import time
+from PIL import ImageTk,Image
+import sys
+from PyQt5.QtWidgets import QApplication
+from datetime import datetime
 import ctypes
+import os
+import urllib
+import io
+#import stockticker
+
 user32 = ctypes.windll.user32
-screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
-print(screensize)
+screensize = str(user32.GetSystemMetrics(0)) + "x" + str(user32.GetSystemMetrics(1))
+normalScreenDPI = 157.74020756611984
+95.95804195804195
+app = QApplication(sys.argv)
+screen = app.screens()[0]
+dpi = screen.physicalDotsPerInch()
+app.quit()
+
+root = Tk()
+root.tk.call('tk', 'scaling', '-displayof', '.', dpi/72.0)
+root.geometry(screensize)
+canvas= Canvas(root,width=root.winfo_screenwidth(),height=root.winfo_screenheight())
+canvas.place(x =0, y = 0)
+canvas.create_text(0,0,text="test",font="bold 550", anchor=NW)
+
+faktor = dpi / normalScreenDPI
+
+print(dpi)
+root.attributes('-fullscreen', True)
+root.mainloop()
