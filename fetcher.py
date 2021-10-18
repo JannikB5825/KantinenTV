e9c14cbb18f5288012dd39306bffc1cc72bc7b01
RollerCoaster4 <jannik.becker@bender.de>
2021-10-18 07:41:58 +0200

    7:41 18.10.2021

--- fetcher.py
+++ fetcher.py
@@ -6,6 +6,7 @@ from io import BytesIO
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import time
+#from requests_ntlm import HttpNtlmAuth
 
 
 class CrawledFetcher():
@@ -19,7 +20,8 @@ class CrawledFetcher():
     return [str(self.title), str(self.content), str(self.author), str(self.date), str(self.image)]
 
 
-class ArticleFetcher():  
+class ArticleFetcher():
+  #bender.de  
   def fetch():
     url = "https://www.bender.de/unternehmen/news"
     articles = [ ]
@@ -35,4 +37,29 @@ class ArticleFetcher():
       articles.append(crawled.selfList())
     return articles[:5]
 
-
+  def fetchIntra():
+    url = "https://intra.mybender.com/de/"
+    articles = [ ]
+    time.sleep(1)
+    r = requests.get(url)
+    with open("intraBender.html") as f:
+      doc = BeautifulSoup(f, "html.parser")
+    for card in doc.select("tr.ms-itmHoverEnabled "):
+      strong = card.select_one("strong").text
+      if "â€‹" in strong:
+        strong = "".join(c for c in strong if ord(c)<128)
+      content = "content"
+      title = card.select_one("p")
+      if type(title) != """NoneType""":
+        title = title.get_text().replace("\xa0"," ")
+        title = "".join(c for c in title if ord(c)<128)[len(strong):]
+      #date = card.select_one("time").text[3:-4]
+      #img = card.select_one("img").get("src")
+      crawled = CrawledFetcher(strong, title, "date", "img")
+      articles.append(crawled.selfList())
+    return articles
+  
+#srticels = ArticleFetcher.fetchIntra()
+#for x in srticels:
+#  print(x[0])
+#  print(x[1])
